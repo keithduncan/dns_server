2140b0e2d3b4634ed10433f59620a5a0dfeb7cf9
Keith Duncan <keith@realmacsoftware.com>
2013-01-26 23:24:59 +0000

    Add more socket option notes Provide server schedule in the constructor, allowing the schedule on the main queue to reside in the same function that calls `dispatch_main()`

--- DNS Server/main.m
+++ DNS Server/main.m
@@ -13,12 +13,9 @@
 #import "AFNetworkDomainServer.h"
 #import "AFNetworkDomainZone.h"
 
-static AFNetworkDomainServer *StartDomainServer(void) {
+static AFNetworkDomainServer *StartDomainServer(AFNetworkSchedule *schedule) {
 	AFNetworkDomainServer *server = [AFNetworkDomainServer server];
-	
-	AFNetworkSchedule *newSchedule = [[[AFNetworkSchedule alloc] init] autorelease];
-	[newSchedule scheduleInQueue:dispatch_get_main_queue()];
-	server.schedule = newSchedule;
+	server.schedule = schedule;
 	
 	/*
 		Internet Layer + Options
@@ -36,19 +33,25 @@ static AFNetworkDomainServer *StartDomainServer(void) {
 		IN6_IS_ADDR_MULTICAST(0);
 		
 		join multicast groups automatically based on whether the address is a multicast group, or provide configurable IP layer options on AFNetworkSocket
+		
+		we need to join the multicast group on all interfaces, therefore we need to track changes to interfaces
+		consult the mDNSResponder code to see what mechanism it uses to monitor interfaces, probably the SystemConfiguration framework
+		
+		can we open a wildcard IPv4/IPv6 socket address and join the group using that, this would avoid tracking interface changes as we avoided tracking interface changes in AFNetworkServer
+		though we still need to track address family availablity changes which we don't current handle in AFNetworkServer :(
 	 */
 #warning this relies on mDNSResponder joining the multicast group, we should join it too so not to rely on that
 	
 	/*
 		Transport Layer + Options
 	 */
-	uint16_t port = 5353;
 	
 	/*
 		Note
 		
 		needs to set SO_REUSEADDR on the socket for the bind to succeed when mDNSResponder is already bound to the port
 	 */
+	uint16_t port = 5353;
 #warning this relies on the debug-only options set in AFNetworkSocket for reuse address and reuse port, these need to be configurable for use in release configuration too for binding these addresses to work in the presence of mDNSResponder
 	
 	NSMutableSet *newAddresses = [NSMutableSet set];
@@ -65,7 +68,10 @@ static AFNetworkDomainServer *StartDomainServer(void) {
 }
 
 void server_main(void) {
-	__unused AFNetworkDomainServer *domainServer = [StartDomainServer() retain];
+	AFNetworkSchedule *newSchedule = [[[AFNetworkSchedule alloc] init] autorelease];
+	[newSchedule scheduleInQueue:dispatch_get_main_queue()];
+	
+	__unused AFNetworkDomainServer *domainServer = [StartDomainServer(newSchedule) retain];
 	
 	AFNetworkDomainZone *zone = [[AFNetworkDomainZone alloc] init];
 	
