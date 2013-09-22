31bcf24b5a12c7b5368892c89cf608111c394a32
Keith Duncan <keith_duncan@me.com>
2013-09-22 15:36:15 +0100

    Add constants file for the bundle identifier

---
+++ DNS Server/DNS Server-Constants.h
@@ -0,0 +1,11 @@
+//
+//  DNS Server-Constants.h
+//  DNS Server
+//
+//  Created by Keith Duncan on 22/09/2013.
+//  Copyright (c) 2013 Keith Duncan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const AFNetworkDomainServerBundleIdentifier;
---
+++ DNS Server/DNS Server-Constants.m
@@ -0,0 +1,11 @@
+//
+//  DNS Server-Constants.m
+//  DNS Server
+//
+//  Created by Keith Duncan on 22/09/2013.
+//  Copyright (c) 2013 Keith Duncan. All rights reserved.
+//
+
+#import "DNS Server-Constants.h"
+
+NSString *const AFNetworkDomainServerBundleIdentifier = @"com.keith-duncan.domain-server";
